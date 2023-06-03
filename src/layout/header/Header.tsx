c09c1b4f23d28f2593a6d03ec9b2a88381c2ed12
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-06-03 20:04:12 +0300

    create prepare header structure

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,36 @@
 export const Header = () => {
   return (
     <header>
-      Header!
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">home</a>
+          </li>
+          <li>
+            <a href="#skills">skills</a>
+          </li>
+          <li>
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contacts">contacts</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        <ul>
+          <li>
+            <a href="#">github icon</a>
+          </li>
+          <li>
+            <a href="#">instagram icon</a>
+          </li>
+          <li>
+            <a href="#">linkedin icon</a>
+          </li>
+        </ul>
+      </div>
     </header>
-  );
-};
+  )
+}
