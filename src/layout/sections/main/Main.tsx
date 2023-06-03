7dcdb61638cdb9983c25778e235c3475d8a0a871
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-06-03 20:05:56 +0300

    create prepare main section structure

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,3 +1,13 @@
+import mainPhoto from '../../../assets/images/mainPhoto.jpg'
 export const Main = () => {
-  return <div>Main</div>
+  return (
+    <section className="home" id="home">
+      <div>
+        <img src={mainPhoto} alt="My photo" />
+        <h3>svyatoslav nemkovich</h3>
+        <p>i am a front-end developer</p>
+        <a href="#">download CV</a>
+      </div>
+    </section>
+  )
 }
